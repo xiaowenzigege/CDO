6447c4ce81a7eaab41cf9ed6e9bfe035859e468a
KenelLiu <KenelLiu@china-liantong.com>
2016-07-04 18:36:41 +0800

    modify URLUtil

--- CDOUtil/src/main/java/com/cdo/util/http/URLUtil.java
+++ CDOUtil/src/main/java/com/cdo/util/http/URLUtil.java
@@ -5,8 +5,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.HashMap;
 import java.util.Map;
 /**
  * 
@@ -14,14 +17,23 @@ import java.util.Map;
  *
  */
 public class URLUtil{
-  private static int TIME_OUT = 1000;
+  private static int TIME_OUT =30*1000;
 
-  public static String getParamsString(Map<String, String> params) {
+  private static String reqParams(Map<String, String> params){
     StringBuffer rtn = new StringBuffer();
+    int index=0;
     for (Map.Entry<String,String> entry : params.entrySet()) {
-      rtn.append("&" +entry.getKey() + "=" +entry.getValue());
+    	if(index>0)
+    		rtn.append("&");
+    	try {
+			rtn.append(entry.getKey() + "="+URLEncoder.encode(entry.getValue(),"UTF-8"));
+		} catch (UnsupportedEncodingException e) {
+			//不支持UTF-8 编码,则使用默认值
+			rtn.append(entry.getKey() + "="+entry.getValue());
+		}
+    	index++;
     }
-    return rtn.toString().replaceFirst("&", "");
+    return rtn.toString();
   }
 
   private static String read(InputStream is) throws IOException {
@@ -41,33 +53,25 @@ public class URLUtil{
     }   
   }
 
-  public static String doPost(String path, Map<String, String> params) throws IOException {
-    path = checkPath(path);
-    URL urlObject = new URL(path);
+  public static String doPost(String url, Map<String, String> params) throws IOException {
+    URL urlConnetion = new URL(url);
     URLConnection conn = null;
     String rtn = null;
     PrintWriter pw = null;
     try {
-      conn = urlObject.openConnection();
-      conn.setReadTimeout(TIME_OUT);
+      conn = urlConnetion.openConnection();
+      conn.setReadTimeout(TIME_OUT);      
       conn.setDoOutput(true);
       conn.setDoInput(true);
       pw = new PrintWriter(conn.getOutputStream(), true);
-      pw.print(getParamsString(params));
+      pw.print(reqParams(params));
       pw.flush();      
-      rtn = read(conn.getInputStream());
+      rtn = read(conn.getInputStream());      
       return rtn;
     }catch(Exception ex){
     	throw new IOException(ex);
   	}finally {
-      if (pw != null) {try{pw.close();}catch(Exception ex){}}
+      if (pw != null) {try{pw.close();}catch(Exception ex){}}      
     }     
   }
-
-  private static String checkPath(String path) {
-    path = path.replaceAll("\\?$", "");
-    path = path + "?";
-    return path;
-  }
-
 }
