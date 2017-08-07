0ad3306622f6ac5ccc947976986f2d587388ef52
KenelLiu <KenelLiu@china-liantong.com>
2017-08-07 15:03:14 +0800

    remove comment for protocol

--- CDOLib/src/main/java/com/cdo/google/handle/ProtoProtocol.java
+++ CDOLib/src/main/java/com/cdo/google/handle/ProtoProtocol.java
@@ -7,7 +7,7 @@ public interface ProtoProtocol extends Protocol{
 	  *  	CDO 协议=魔数(2个字节)+消息类型(1个字节)+对象内容长度(3个字节)	
 	  *  	  * 
 	  * 消息为 TYPE_HEARTBEAT_REQ,TYPE_HEARTBEAT_RES 心跳检测类型,则无实际内容传输，即不存在CDO对象
-	  * 内容长度=3个字节   包含1个字节消息类型+CDO内容的长度
+	  * 
 	  * 
 	  */
 	 final int   PROTOCOL_LEN=2+1+3;
